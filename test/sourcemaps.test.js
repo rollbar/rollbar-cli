7ac2a0736f3fbe1e3ed748481bf1597a5805b03b
Walt Jones <waltjones@gmail.com>
2020-08-21 08:37:32 -0700

    add test automation and coverage report

---
+++ test/sourcemaps.test.js
@@ -0,0 +1,31 @@
+/* globals describe */
+/* globals it */
+
+const expect = require('chai').expect;
+const exec = require('child_process').exec;
+const execSync = require('child_process').execSync;
+
+describe('rollbar-cli upload-sourcemaps', function() {
+  it('returns help output', done => {
+    this.timeout(5000);
+
+    exec('./bin/rollbar upload-sourcemaps --help', 'utf8', (_err, stdout, _stderr) => {
+      expect(stdout).to.have.string('upload-sourcemaps <path> [options]');
+      done();
+    })
+  });
+
+  it('uploads react project', function() {
+    this.timeout(5000);
+
+    const stdout = execSync('./bin/rollbar upload-sourcemaps ./test/fixtures/builds/react16/build --access-token 1234 --url-prefix "http://localhost:3000/" --code-version react16 -D');
+
+    const lines = stdout.toString().split('\n');
+
+    expect(lines.length).to.equal(14);
+
+    for(let i; i < lines.length; i+=2) {
+      expect(lines[i]).to.have.string('[Found ]');
+    }
+  });
+});
---
+++ test/sourcemaps/scanner.test.js
@@ -0,0 +1,129 @@
+/* globals describe */
+/* globals it */
+/* globals beforeEach */
+
+const expect = require('chai').expect;
+//const sinon = require('sinon');
+
+const Scanner = require('../../src/sourcemaps/scanner');
+const Output = require('../../src/common/output');
+
+describe('Scanner()', function() {
+  beforeEach(function(){
+    global.output = new Output({verbose: false});
+  })
+
+  it('should initialize successfully', function() {
+    const options = {
+      targetPath: 'foo',
+      sources: true
+    };
+    const scanner = new Scanner(options);
+
+    expect(scanner.targetPath).to.equal(options.targetPath);
+    expect(scanner.sources).to.equal(options.sources);
+    expect(scanner.projectPath).to.equal('./');
+    expect(scanner.files).to.be.an('array').that.is.empty;
+    expect(scanner.mappings).to.be.an('object').that.is.empty;
+  });
+});
+
+describe('.scan()', function() {
+  it('should scan and load files from a directory', async function() {
+    const options = {
+      targetPath: './test/fixtures/builds/react16/build',
+      sources: true
+    };
+    const scanner = new Scanner(options);
+    await scanner.scan();
+    const files = scanner.mappedFiles();
+
+    expect(files.length).to.equal(5);
+
+    // First two files not source mapped
+    expect(files[0].fileName).to.have.string('precache-manifest');
+    expect(files[0].sourceMappingURL).to.be.false;
+    expect(files[0].mapPathName).to.be.null;
+    expect(files[0].errors).to.be.an('array').that.is.empty;
+
+    expect(files[1].fileName).to.have.string('service-worker.js');
+    expect(files[1].sourceMappingURL).to.be.false;
+    expect(files[1].mapPathName).to.be.null;
+    expect(files[1].errors).to.be.an('array').that.is.empty;
+
+    // Last three files are source mapped
+    expect(files[2].fileName).to.have.string('2.373ccd69.chunk.js');
+    expect(files[2].sourceMappingURL).to.be.true;
+    expect(files[2].mapPathName).to.have.string('2.373ccd69.chunk.js.map');
+    expect(files[2].metadata.version).to.equal(3);
+    expect(files[2].metadata.sources.length).to.equal(16);
+    expect(files[2].errors).to.be.an('array').that.is.empty;
+
+    expect(files[3].fileName).to.have.string('main.d504f0ad.chunk.js');
+    expect(files[3].sourceMappingURL).to.be.true;
+    expect(files[3].mapPathName).to.have.string('main.d504f0ad.chunk.js.map');
+    expect(files[3].metadata.version).to.equal(3);
+    expect(files[3].metadata.sources.length).to.equal(6);
+    expect(files[3].errors).to.be.an('array').that.is.empty;
+
+    expect(files[4].fileName).to.have.string('runtime~main.a8a9905a.js');
+    expect(files[4].sourceMappingURL).to.be.true;
+    expect(files[4].mapPathName).to.have.string('runtime~main.a8a9905a.js.map');
+    expect(files[4].metadata.version).to.equal(3);
+    expect(files[4].metadata.sources.length).to.equal(1);
+    expect(files[4].errors).to.be.an('array').that.is.empty;
+  });
+
+  it('should handle errors loading files from a directory', async function() {
+    const options = {
+      targetPath: './test/fixtures/builds/angular9',
+      sources: true
+    };
+    const scanner = new Scanner(options);
+    await scanner.scan();
+    const files = scanner.mappedFiles();
+
+    expect(files.length).to.equal(10);
+
+    expect(files[0].fileName).to.have.string('main-es2015.js');
+    expect(files[0].sourceMappingURL).to.be.false;
+    expect(files[0].errors).to.be.an('array').that.is.empty;
+
+    expect(files[1].fileName).to.have.string('main-es5.js');
+    expect(files[1].sourceMappingURL).to.be.true;
+    expect(files[1].errors).to.be.an('array').that.is.empty;
+
+    expect(files[2].fileName).to.have.string('polyfills-es2015.js');
+    expect(files[2].sourceMappingURL).to.be.false;
+    expect(files[2].errors).to.be.an('array').that.is.empty;
+
+    expect(files[3].fileName).to.have.string('polyfills-es5.js');
+    expect(files[3].sourceMappingURL).to.be.true;
+    expect(files[3].errors).to.be.an('array').that.is.empty;
+
+    expect(files[4].fileName).to.have.string('runtime-es2015.js');
+    expect(files[4].sourceMappingURL).to.be.false;
+    expect(files[4].errors).to.be.an('array').that.is.empty;
+
+    expect(files[5].fileName).to.have.string('runtime-es5.js');
+    expect(files[5].sourceMappingURL).to.be.true;
+    expect(files[5].errors).to.be.an('array').that.is.empty;
+
+    expect(files[6].fileName).to.have.string('styles-es2015.js');
+    expect(files[6].sourceMappingURL).to.be.false;
+    expect(files[6].errors).to.be.an('array').that.is.empty;
+
+    expect(files[7].fileName).to.have.string('styles-es5.js');
+    expect(files[7].sourceMappingURL).to.be.true;
+    expect(files[7].errors).to.be.an('array').that.is.empty;
+
+    expect(files[8].fileName).to.have.string('vendor-es2015.js');
+    expect(files[8].sourceMappingURL).to.be.true;
+    expect(files[8].errors.length).to.equal(1);
+    expect(files[8].errors[0].error).to.have.string('Error parsing map file');
+
+    expect(files[9].fileName).to.have.string('vendor-es5.js');
+    expect(files[9].sourceMappingURL).to.be.true;
+    expect(files[9].errors).to.be.an('array').that.is.empty;
+  });
+});
