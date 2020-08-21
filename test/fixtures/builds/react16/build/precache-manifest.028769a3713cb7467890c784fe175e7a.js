b42a73532e4dab47c8f2ad2808e07fc7084580b6
Walt Jones <waltjones@gmail.com>
2020-08-21 08:36:27 -0700

    add fixtures (representative source map builds)

---
+++ test/fixtures/builds/react16/build/precache-manifest.028769a3713cb7467890c784fe175e7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51fe79ae8f98a5878e07539f61f178f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1da5214fcb1322cf679",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e1df9e5dd97f9dc5b41a",
+    "url": "/static/js/2.373ccd69.chunk.js"
+  },
+  {
+    "revision": "b1da5214fcb1322cf679",
+    "url": "/static/js/main.d504f0ad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
