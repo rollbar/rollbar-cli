a1d571a6955abc1a4fcd024c5dea05044d014381
Mrunal Kapade <mrunal@rollbar.com>
2020-09-10 00:47:54 -0700

    Update readme and add local_username and comment fields

--- test/deploy.test.js
+++ test/deploy.test.js
@@ -17,7 +17,7 @@ describe('rollbar-cli notify-deploy', function() {
   it('should handle error response', done => {
     this.timeout(5000);
 
-    const stdout = execSync('./bin/rollbar notify-deploy --access-token 1234 --code-version 1.0.1 --environment production --username foobar --status succeeded --deploy-id 12345678');
+    const stdout = execSync('./bin/rollbar notify-deploy --access-token 1234 --code-version 1.0.1 --environment production --rollbar-username foobar --status succeeded --deploy-id 12345678 --local-username foo_bar --comment \'Deploy Test\'');
 
     const lines = stdout.toString().split('\n');
 
