f40379af9c92bc323ea822fc309d6fee5965b144
Pawel Szczodruch <pawel.szczodruch@rollbar.com>
2021-02-25 16:53:07 -0800

    ident again

--- src/sourcemaps/uploader.js
+++ src/sourcemaps/uploader.js
@@ -5,83 +5,83 @@ const URL = require('url').URL;
 
 class Uploader {
   constructor(options) {
-      this.files = [];
-      this.rollbarAPI = new RollbarAPI(options.accessToken);
-      this.baseUrl = options.baseUrl;
-      this.version = options.codeVersion;
+    this.files = [];
+    this.rollbarAPI = new RollbarAPI(options.accessToken);
+    this.baseUrl = options.baseUrl;
+    this.version = options.codeVersion;
   }
 
-    mapFiles(files) {
-        this.files = files;
+  mapFiles(files) {
+    this.files = files;
 
-        return this;
+    return this;
+  }
+
+  async upload(dryRun) {
+    if (dryRun) {
+      // TODO: Maybe more can be done here, but the important part is just to
+      // return without sending. The bulk of validation is done earlier
+      // in the scanning phase.
+      return this.files;
     }
 
-    async upload(dryRun) {
-        if (dryRun) {
-            // TODO: Maybe more can be done here, but the important part is just to
-            // return without sending. The bulk of validation is done earlier
-            // in the scanning phase.
-            return this.files;
+    for (const file of this.files) {
+      output.status('Upload', file.mapPathName);
+
+      try {
+        if (this.skip(file)) {
+          continue;
         }
 
-        for (const file of this.files) {
-            output.status('Upload', file.mapPathName);
+        const error = await this.rollbarAPI.sourcemaps(this.buildRequest(file));
 
-            try {
-                if (this.skip(file)) {
-                    continue;
-                }
-
-                const error = await this.rollbarAPI.sourcemaps(this.buildRequest(file));
-
-                if (error) {
-                    file.errors.push({
-                        error: error.message,
-                        file: file
-                    });
-                }
-            } catch (e) {
-                file.errors.push({
-                    error: e.message,
-                    file: file
-                });
-            }
-
-            if (!file.errors.length) {
-                output.success('', 'Upload successful');
-            }
-
-            for (const error of file.errors) {
-                output.error('Error', error.error);
-            }
+        if (error) {
+          file.errors.push({
+            error: error.message,
+            file: file
+          });
         }
+      } catch (e) {
+        file.errors.push({
+          error: e.message,
+          file: file
+        });
+      }
 
-        return this.files;
+      if (!file.errors.length) {
+        output.success('', 'Upload successful');
+      }
+
+      for (const error of file.errors) {
+        output.error('Error', error.error);
+      }
     }
 
-    buildRequest(file) {
-        return {
-            version: this.version,
-            minified_url: this.minifiedUrl(file.fileName),
-            source_map: file.mapData,
-            sources: file.sources
-        }
+    return this.files;
+  }
+
+  buildRequest(file) {
+    return {
+      version: this.version,
+      minified_url: this.minifiedUrl(file.fileName),
+      source_map: file.mapData,
+      sources: file.sources
+    }
+  }
+
+  minifiedUrl(fileName) {
+    return (new URL(fileName, this.baseUrl)).toString();
+  }
+
+  skip(file) {
+    if (file.fileName && file.mapData && !file.errors.length) {
+      return false;
     }
 
-    minifiedUrl(fileName) {
-        return (new URL(fileName, this.baseUrl)).toString();
-    }
+    output.warn('', 'skip: ' + file.filePathName);
 
-    skip(file) {
-        if (file.fileName && file.mapData && !file.errors.length){
-            return false;
-        }
-
-        output.warn('', 'skip: ' + file.filePathName);
-
-        return true;
-    }
+    return true;
+  }
 }
 
 module.exports = Uploader;
